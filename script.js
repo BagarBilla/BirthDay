ad4b81df82f7cec4a5d90a4b929bdea6fcbf5212
BagarBilla <133000607+BagarBilla@users.noreply.github.com>
2025-03-08 22:05:51 +0530

    files added

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    // Hide the initial view
+    document.getElementById("initialView").style.display = "none";
+
+    // Show the celebration view
+    document.getElementById("celebrationView").style.display = "flex";
+});
